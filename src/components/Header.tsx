4120f7e75cab89060efa13f48c342ceb9cd7bdd5
Silvia rodriguez <silvideastora18@gmail.com>
2023-08-08 02:02:57 -0600

    fix title styles on header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,7 +7,7 @@ interface Props {
 export const Header: React.FC<Props> = ({ onAddTodo }) => {
   return(
     <header className="header">
-      <h2>To o con Typescript</h2>
+      <h2>To Do con Typescript</h2>
       <CreateTodo 
         saveTodo={onAddTodo}
       />
